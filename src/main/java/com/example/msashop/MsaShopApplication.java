a33fac01807876ac63e2e59d9f372d68f419122b
junyeong <gowjr207@gmail.com>
2022-05-26 18:47:21 +0900

    Initial commit (swagger, feign, eureka add)

---
+++ src/main/java/com/example/msashop/MsaShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.msashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/msashop/MsaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
