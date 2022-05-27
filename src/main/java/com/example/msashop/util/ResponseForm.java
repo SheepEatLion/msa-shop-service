3b291cfa1641d097ee1bffda596c489c7e1f695e
junyeong <gowjr207@gmail.com>
2022-05-27 14:43:43 +0900

    fix: feign return mapping을 위한 생성자 추가

--- src/main/java/com/example/msashop/util/ResponseForm.java
+++ src/main/java/com/example/msashop/util/ResponseForm.java
@@ -1,14 +1,14 @@
 package com.example.msashop.util;
 
 import com.example.msashop.domain.Shop;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
 @Setter
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class ResponseForm <T>{
 
